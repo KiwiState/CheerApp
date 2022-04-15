d6d11578ee0f8f54037320e4892512acd2481d9c
NicoVeroitza <103152373+NicoVeroitza@users.noreply.github.com>
2022-04-14 21:18:16 -0400

    Prueba en UI

--- app/src/main/java/com/example/cheerapp/ConexionBD.java
+++ app/src/main/java/com/example/cheerapp/ConexionBD.java
@@ -1,26 +1,34 @@
 package com.example.cheerapp;
 
+import android.annotation.SuppressLint;
+
 import java.sql.SQLException;
 import java.sql.DriverManager;
 import java.sql.Connection;
 
+@SuppressLint("NewApi")
 public class ConexionBD {
 
-    String URL = "jdbc:oracle:thin:@cheerapdb_high?TNS_ADMIN=app/Wallet_CheerAppDB";
+    String URL = "jdbc:oracle:thin:@cheerappdb_high?TNS_ADMIN=app/Wallet_CheerAppDB";
     String user = "DEVELOPER";
     String password = "Cheerapp1234";
+    Connection conexion = null;
 
-    public void Conector(){
+
+    public Connection Conector(){
 
         try {
 
-            Connection conexion = DriverManager.getConnection(URL, user, password);
+            conexion = DriverManager.getConnection(URL, user, password);
             System.out.println("Conectado a base de datos Oracle");
+
 
         }catch(SQLException e){
             System.out.println("Error de conexion");
             e.printStackTrace();
 
         }
+    return conexion;
+
     }
 }
