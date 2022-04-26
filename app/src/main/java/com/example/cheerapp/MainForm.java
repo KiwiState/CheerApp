2f0385cc6f41c0883cac2b58e58d277d58318b90
KiwiState <vicentegandolfo@gmail.com>
2022-04-25 22:34:17 -0400

    nueva ventana y formulario

--- app/src/main/java/com/example/cheerapp/MainForm.java
+++ app/src/main/java/com/example/cheerapp/MainForm.java
@@ -1,51 +1,46 @@
 package com.example.cheerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.hsalf.smilerating.SmileRating;
+public class MainForm extends AppCompatActivity {
+    private Button btn_enviar;
+    private EditText edit_text;
+    private String strDesc;
 
-import org.json.JSONArray;
-
-public class MainActivity extends AppCompatActivity {
-    private Button btn_emo;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btn_emo = findViewById(R.id.btn_emotion);
-        btn_emo.setOnClickListener(new View.OnClickListener(){
+        setContentView(R.layout.activity_main_form);
+
+        btn_enviar = findViewById(R.id.btns_save);
+        edit_text = (EditText) findViewById(R.id.edittext_desc);
+        strDesc = edit_text.getText().toString();
+
+        btn_enviar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this,MainForm.class);
-                startActivity(i);
             }
         });
-
-
 
     }
 
     private void showDialog(){
         Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.dialog_main);
-
         //dialog.getWindow().setBackgroundDrawableResource(R.drawable.gradient_background);
-
         dialog.show();
 
     }
+
 
 }
