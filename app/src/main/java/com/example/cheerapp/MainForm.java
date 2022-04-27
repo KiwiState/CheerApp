2db59cae5b8c1909d6911e046bb93632286b352d
KiwiState <vicentegandolfo@gmail.com>
2022-04-26 21:25:14 -0400

    Save storage
    Falta guardar en array y descubrir como guardar los datos del raiting

--- app/src/main/java/com/example/cheerapp/MainForm.java
+++ app/src/main/java/com/example/cheerapp/MainForm.java
@@ -5,32 +5,85 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class MainForm extends AppCompatActivity {
+    private static final String FILE_NAME = "example.txt";
     private Button btn_enviar;
-    private EditText edit_text;
-    private String strDesc;
-
-
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_form);
-
+        mEditText =  findViewById(R.id.edittext_desc);//ID del texto de como te siente
         btn_enviar = findViewById(R.id.btns_save);
-        edit_text = (EditText) findViewById(R.id.edittext_desc);
-        strDesc = edit_text.getText().toString();
 
-        btn_enviar.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
+    }
+    public  void save(View v) throws IOException {
+        String text = mEditText.getText().toString();// texto de como te siente
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(FILE_NAME,MODE_PRIVATE);
+            fos.write(text.getBytes());
+            mEditText.getText().clear();
+            Toast.makeText(this,"Saved to"+ getFilesDir()+"/"+FILE_NAME,Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fos != null){
+                try{
+                    fos.close();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
             }
-        });
+        }
+    }
+
+    public  void load(View v) {
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            while ((text = br.readLine()) != null ){
+                sb.append(text).append("\n");
+            }
+
+            mEditText.setText(sb.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            if(fis != null){
+                try{
+                    fis.close();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 
